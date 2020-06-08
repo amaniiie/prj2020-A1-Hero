27b9b186879c84feab6f358b8c3fa1910235e26e
narinda.ada@student.mahidol.ac.th <narinda.ada@student.mahidol.ac.th>
2020-06-08 23:14:44 +0700

    some changes + build

---
+++ Assets/Rsc_Hero/Script/ReScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReScene : MonoBehaviour
+{
+    public void RestartGame(){
+        SceneManager.LoadScene("Starter2DSceneLV2");
+        // Application.LoadLevel(Application.loadedLevel);
+    }
+
+}
